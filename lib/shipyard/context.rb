debbfb643dd69fd3fc105154e87d5527f1486ad4
Chris Kite <chris@chriskite.com>
2010-07-14 12:28:08 -0500

    convert columns to strings in context

--- lib/shipyard/context.rb
+++ lib/shipyard/context.rb
@@ -11,15 +11,15 @@ class Context
   end
 
   def columns
-    columns_with_types.keys
+    columns_with_types.keys.map(&:to_s)
   end
 
   def columns_with_types
-    @db.schema(@table.to_sym).inject({}) { |r, v| r[v[0]] = v[1][:type]; r }
+    @db.schema(@table.to_sym).inject({}) { |r, v| r[v[0].to_s] = v[1][:type].to_s; r }
   end
 
   def primary_key
-    @db.schema(@table.to_sym).select { |col_arr| col_arr[1][:primary_key] == true }.map { |col_arr| col_arr[0] }
+    @db.schema(@table.to_sym).select { |col_arr| col_arr[1][:primary_key] == true }.map { |col_arr| col_arr[0].to_s }.first
   end
 
   def get_binding
